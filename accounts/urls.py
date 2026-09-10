832efadc5eb4d3e5054328a589b5506958573052
Boshraraamandi <boshra.fasihi.ramandi@gmail.com>
2026-09-10 10:38:16 +0330

    Update accounts views and urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,40 @@
 from django.urls import path
+
 from .views import (
-    LoginView, 
-    LogoutView, 
+    LoginView,
+    LogoutView,
     CurrentUserView,
-    CheckAuthView
+    CheckAuthView,
 )
+
 
 app_name = 'accounts'
 
+
 urlpatterns = [
-    path('api/login/', LoginView.as_view(), name='login'),
-    path('api/logout/', LogoutView.as_view(), name='logout'),
-    path('api/user/', CurrentUserView.as_view(), name='current-user'),
-    path('api/check-auth/', CheckAuthView.as_view(), name='check-auth'),
-]
+
+    path(
+        'api/login/',
+        LoginView.as_view(),
+        name='login'
+    ),
+
+    path(
+        'api/logout/',
+        LogoutView.as_view(),
+        name='logout'
+    ),
+
+    path(
+        'api/user/',
+        CurrentUserView.as_view(),
+        name='current-user'
+    ),
+
+    path(
+        'api/check-auth/',
+        CheckAuthView.as_view(),
+        name='check-auth'
+    ),
+
+]      
