ad29ee1db3ce68e5c70f1b4a703a5c494732db30
mohammad-hossein-daei <m.hosein.daei82@gmail.com>
2026-09-13 15:15:31 +0330

    update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',   
-    
+    'drf_spectacular',
+        
     'accounts',
     'core',
     'referrals',
@@ -170,5 +171,6 @@ REST_FRAMEWORK = {
     ],
     # این خط باعث می‌شود پاسخ unauthenticated با 401 برگردد
     'EXCEPTION_HANDLER': 'accounts.exceptions.custom_exception_handler',
-
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.middleware.csrf import get_token
 from django.http import JsonResponse
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 def csrf_view(request):
@@ -17,11 +18,16 @@ def admin_logout_redirect(request):
 
 
 urlpatterns = [
-    # ⚠️ مهم: قبل از admin/ ثبت شود
     path('admin/logout/', admin_logout_redirect, name='admin-logout'),
 
     path('admin/', admin.site.urls),
     path('api/csrf/', csrf_view, name='csrf'),
     path('', include('accounts.urls')),
     path('', include('referrals.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
 ]
